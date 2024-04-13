d2fb8a9932954f9ecdc3ff01380a04fdf83c6b17
Guilherme da Costa <124263467+Guilherme-daCosta@users.noreply.github.com>
2024-04-13 00:25:21 +0000

    add comentary for my wife (challenge)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Testeeeee</h1>
+    return 
+        <div>
+            <h1>Só queria passar para deixar um recado:</h1>
+            <h3>Tenho muito orgulho de você e de estar ao seu lado</h3>
+            <h2>Te amo muitaoooooooooo</h2>
+        </div>
 }
 
 
