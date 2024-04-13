2f46c07f97a22be61485c34cc969e24b8c330d11
Guilherme da Costa <124263467+Guilherme-daCosta@users.noreply.github.com>
2024-04-13 23:37:29 +0000

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 function Home() {
-    return (
-        <div>
-            <h1>Só queria passar para deixar um recado:</h1>
-            <h3>Tenho muito orgulho de você e de estar ao seu lado</h3>
-            <h2>Te amo muitaoooooooooo</h2>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Só queria passar para deixar um recado:</h1>
+      <h3>Tenho muito orgulho de você e de estar ao seu lado</h3>
+      <h2>Te amo muitaoooooooooo</h2>
+    </div>
+  );
 }
-
 
 export default Home;
